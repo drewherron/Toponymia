9cec2b010a3ce4ae705b3c1157a5acc5842d4855
Drew Herron <herron@mailbox.org>
2019-04-29 16:35:00 -0700

    Temp update

--- topoproj/topoproj/settings.py
+++ topoproj/topoproj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import secret_settings
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'topoproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_KEY',
-        'HOST': 'DB_HOST',
-        'PORT': 'DB_PORT',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_KEY,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
