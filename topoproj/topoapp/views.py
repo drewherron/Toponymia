3b133089ace97c85ea8057e08922f8d36b3a1f1f
Drew Herron <herron@mailbox.org>
2019-04-30 11:52:32 -0700

    Make minor initial edits

--- topoproj/topoapp/views.py
+++ topoproj/topoapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'toponymia/index.html')
+    return render(request, 'topoapp/index.html')
+
+def psql(request):
+    return render(request, 'topoapp/psql.html')
