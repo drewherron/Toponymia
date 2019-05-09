2d8ab3e1f674c711c823fa772e878230add45464
Drew Herron <herron@mailbox.org>
2019-05-09 16:31:00 -0700

    Remove unnecessary urls

--- proj_tpnm/app_tpnm/urls.py
+++ proj_tpnm/app_tpnm/urls.py
@@ -5,8 +5,5 @@ from . import views
 app_name = 'app_tpnm'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new_article/', views.new_article, name='new_article'),
-    path('edit_article/', views.edit_article, name='edit_article'),
-    path('indextest/', views.indextest, name='indextest'),
     path('save_article/', views.save_article, name='save_article'),
 ]
