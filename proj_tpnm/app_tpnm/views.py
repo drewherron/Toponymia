7ee5e67fadeefd7c371db233803e6352a41be6df
Drew Herron <herron@mailbox.org>
2019-05-09 16:30:44 -0700

    Remove unnecessary views

--- proj_tpnm/app_tpnm/views.py
+++ proj_tpnm/app_tpnm/views.py
@@ -7,31 +7,12 @@ import json
 
 
 def index(request):
-    # marker_locations = Article.objects.get()
-    # post_json = json.loads(request.body)
     context = {
     #     'MAP_KEY': settings.MAP_KEY,
     }
     return render(request, 'app_tpnm/index.html', context)
 
 def save_article(request):
-    data = json.loads(request.body)
-    print(data)
-    # text = data['text']
-    # ajax_demo_data = Article(title=title, etc)
+    # data = json.loads(request.body)
+    # print(data)
     return HttpResponse('saved')
-
-def new_article(request):
-    data = json.loads(request.body)
-    print(data)
-    return render (request, 'app_tpnm/new_article.html')
-    # return HttpResponseRedirect(reverse('app_tpnm:index'))
-
-def edit_article(request):
-    context = {
-        'message': 'test_message',
-    }
-    return render (request, 'app_tpnm/edit.html', context)
-
-def indextest(request):
-    return render (request, 'app_tpnm/indextest.html')
