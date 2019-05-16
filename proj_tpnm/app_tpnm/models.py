7595d70c5b04a8c38c058f224a0811de82d6e83a
Drew Herron <herron@mailbox.org>
2019-05-16 11:12:28 -0700

    Change __str__ on Article

--- proj_tpnm/app_tpnm/models.py
+++ proj_tpnm/app_tpnm/models.py
@@ -23,7 +23,7 @@ class Article(models.Model):
     reported_by = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
-        return self.title + self.iso_3166_1 + self.tpnm_id
+        return self.title + ' ' + self.tpnm_id
 
 
 class Edit(models.Model):
