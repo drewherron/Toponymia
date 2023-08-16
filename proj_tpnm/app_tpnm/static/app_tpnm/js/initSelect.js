3bfa193c05dce9a608bbe162cec9a434ab1e5449
Drew Herron <herron@mailbox.org>
2023-08-15 23:16:39 -0700

    Select2 lines in head to new file

---
+++ proj_tpnm/app_tpnm/static/app_tpnm/js/initSelect.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.inlang-multiselect').select2();
+    $('.fromlang-multiselect').select2();
+    $('.nn-inlang-multiselect').select2();
+    $('.nn-fromlang-multiselect').select2();
+    $('.ne-inlang-multiselect').select2();
+    $('.ne-fromlang-multiselect').select2();
+});
